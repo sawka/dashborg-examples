2caef0da7c45c8ffa419270a2af5153135a0a5fe
sawka <mike.sawka@gmail.com>
2021-10-20 12:58:17 -0700

    updating todo.py for new SDK

--- python/todo/todo.py
+++ python/todo/todo.py
@@ -6,17 +6,12 @@ class TodoModel:
         self.todo_list = []
         self.next_id = 1
 
-    async def root_handler(self, req):
-        await req.set_html_from_file("panels/todo.html")
-        return
-
-    async def get_todo_list(self, datareq):
+    async def get_todo_list(self):
         return self.todo_list
 
-    async def add_todo(self, req):
-        req.set_data("$.errors", None)
-        newtodo = req.panel_state.get("newtodo")
-        todo_type = req.panel_state.get("todotype")
+    async def add_todo(self, req, appstate):
+        newtodo = req.app_state.get("newtodo")
+        todo_type = req.app_state.get("todotype")
         if newtodo is None or newtodo == "":
             req.set_data("$.errors", "Please enter a Todo Item")
             return
@@ -26,41 +21,38 @@ class TodoModel:
         todo = {"Id": self.next_id, "Item": newtodo, "TodoType": todo_type, "Done": False}
         self.next_id += 1
         self.todo_list.append(todo)
-        req.invalidate_data("/get-todo-list")
-        req.set_data("$state.newtodo", "")
+        req.invalidate_data("get-todo-list")
         return
 
-    async def mark_todo_done(self, req):
-        if req.data is None:
-            return
-        todo_id = int(req.data)
+    async def mark_todo_done(self, req, todo_id : int):
         for t in self.todo_list:
             if t["Id"] == todo_id:
                 t["Done"] = True
-        req.invalidate_data("/get-todo-list")
+        req.invalidate_data("get-todo-list")
         return
 
-    async def remove_todo(self, req):
-        if req.data is None:
-            return
-        todo_id = int(req.data)
+    async def remove_todo(self, req, todo_id : int):
         self.todo_list = [t for t in self.todo_list if t["Id"] != todo_id]
-        req.invalidate_data("/get-todo-list")
+        req.invalidate_data("get-todo-list")
         return
         
 
 async def main():
     config = dashborg.Config(proc_name="todo", anon_acc=True, auto_keygen=True)
-    await dashborg.start_proc_client(config)
+    client = await dashborg.connect_client(config)
     m = TodoModel()
-    await dashborg.register_panel_handler("todo", "/", m.root_handler)
-    await dashborg.register_panel_handler("todo", "/add-todo", m.add_todo)
-    await dashborg.register_panel_handler("todo", "/mark-todo-done", m.mark_todo_done)
-    await dashborg.register_panel_handler("todo", "/remove-todo", m.remove_todo)
-    await dashborg.register_data_handler("todo", "/get-todo-list", m.get_todo_list)
-    while True:
-        await asyncio.sleep(1)
+    app = client.app_client().new_app("todo")
+    app.set_html(file_name="panels/todo.html", watch=True)
+    app.runtime.set_handler("get-todo-list", m.get_todo_list, pure_handler=True)
+    app.runtime.set_handler("add-todo", m.add_todo)
+    app.runtime.set_handler("mark-todo-done", m.mark_todo_done)
+    app.runtime.set_handler("remove-todo", m.remove_todo)
+    await client.app_client().write_app(app, connect=True)
+    await client.wait_for_shutdown()
 
-asyncio.run(main())
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    pass
 
 
